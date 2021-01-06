2b7b0e61e686735144fa21179bebdcff22cda957
MathiasKrarupMadsen <Math249e@easv365.dk>
2021-01-06 16:07:29 +0100

    puuuush

--- src/sample/gui/controller/MainWindowController.java
+++ src/sample/gui/controller/MainWindowController.java
@@ -2,11 +2,17 @@ package sample.gui.controller;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class MainWindowController {
 
     public Button closeButton;
 
     public void handleSearchMovie(ActionEvent actionEvent) {
+    }
+
+    public void handleClose(ActionEvent actionEvent) {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
     }
 }
