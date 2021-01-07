951f98bc9100d2226b6f8bcba64ca792dcf35e8e
Zacharias Nielsen <zach0066@gmail.com>
2021-01-07 13:52:49 +0100

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-//jensharenstorpik
+
 
     public static void main(String[] args) {
         launch(args);
