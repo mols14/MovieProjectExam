83a9c81d80a98402eac6747d5306f579ebf5c885
Mathias Mols Thiim <math75n9@easv.dk>
2021-01-07 12:28:14 +0100

    Update Filter.java

--- src/sample/bll/util/Filter.java
+++ src/sample/bll/util/Filter.java
@@ -11,7 +11,16 @@ public class Filter {
 
         List<Movie> filterResult = new ArrayList<>();
         for (Movie movie : filterBase) {
-            if(compareToMovieTitle(query, movie) || compareToMovie)
+            if(compareToMovieTitle(query, movie) || compareToMovieCategory)
         }
     }
+
+    private boolean compareToMovieTitle(String query, Movie movie) {
+        return movie.getTitle().toLowerCase().contains(query.toLowerCase());
+    }
+
+    private boolean compareToMovieCategory(String query, Movie movie) {
+        return category.getCategory().toLowerCase().contains(query.toLowerCase());
+    }
+
 }
