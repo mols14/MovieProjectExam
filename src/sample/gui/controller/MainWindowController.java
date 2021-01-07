0a3d57a5f2c3c61cbe887fe6413a67094d96d638
MathiasKrarupMadsen <Math249e@easv365.dk>
2021-01-07 14:12:03 +0100

    ja tak

--- src/sample/gui/controller/MainWindowController.java
+++ src/sample/gui/controller/MainWindowController.java
@@ -2,6 +2,10 @@ package sample.gui.controller;
 
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
@@ -28,5 +32,14 @@ public class MainWindowController implements initializable {
     public void handleClose(ActionEvent actionEvent) {
         Stage stage = (Stage) closeButton.getScene().getWindow();
         stage.close();
+    }
+
+    public void clickNewCategory(ActionEvent actionEvent) {
+
+        Parent mainWindowParent = FXMLLoader.load(getClass().getResource("/GUI/View/NewSong.fxml")); // Path til FXML filen der tilhøre scenen der skal vises
+        Scene mainWindowScene = new Scene(mainWindowParent); //Scenen der skal vises
+        Stage newSongStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        newSongStage.setScene(mainWindowScene); // Sætter nuværende scene
+        newSongStage.show(); // Viser Scenen som lige er blevet sat ovenover
     }
 }
