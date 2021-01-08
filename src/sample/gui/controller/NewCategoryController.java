5d6be93f697699f763399830c35118c1324fab68
MathiasKrarupMadsen <Math249e@easv365.dk>
2021-01-08 15:03:09 +0100

    pushy

--- src/sample/gui/controller/NewCategoryController.java
+++ src/sample/gui/controller/NewCategoryController.java
@@ -1,4 +1,20 @@
 package sample.gui.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class NewCategoryController {
+    public void cancelCategory(javafx.event.ActionEvent event) throws IOException {
+        Parent newSongWindow = FXMLLoader.load(getClass().getResource("/sample/gui/view/MainWindow.fxml")); // Path til fxml filen der tilhøre scenen som skal vises ved tryk på knap
+        Scene newSongScene = new Scene(newSongWindow); // Ny scene som skal vises oprettes
+        Stage mainWindowStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        mainWindowStage.setScene(newSongScene); // Sætter den nye scene
+        mainWindowStage.show(); // Viser den nye scene
+    }
 }
--- src/sample/gui/model/CategoryModel.java
+++ src/sample/gui/model/CategoryModel.java
@@ -1,5 +1,27 @@
 package sample.gui.model;
 
-public class CategoryModel {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import sample.be.Category;
+import sample.bll.MyMovieManager;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class CategoryModel {
+private MyMovieManager myMovieManager;
+private ObservableList <Category> allCategories = FXCollections.observableArrayList();
+public CategoryModel() throws IOException {
+    myMovieManager  = new MyMovieManager();
 }
+
+    public ObservableList<Category> getCategories() throws IOException {
+       allCategories = FXCollections.observableArrayList();
+       allCategories.addAll(myMovieManager.getAllCategories());
+        return allCategories;
+    }
+    public void createCategory(String name) throws SQLException {
+        myMovieManager.createCategory(name);
+    }
+}
+
