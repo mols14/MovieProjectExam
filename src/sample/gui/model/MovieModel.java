6e50cfade4b0af48d48036f79e6ab0b9c68e1cd6
Jens Bassem Hanna Issa <jens7391@easv365.dk>
2021-01-18 10:49:35 +0100

    u

--- src/sample/gui/model/MovieModel.java
+++ src/sample/gui/model/MovieModel.java
@@ -23,7 +23,7 @@ public class MovieModel {
     Opretter ObservableList med film objekter
     således at den kan opdateres løbende. Henter allesange fra manageren
 */
-    
+
     public ObservableList<Movie> getMovies() throws IOException {
         allMovies = FXCollections.observableArrayList();
         allMovies.addAll(myMovieManager.getAllMovies());
